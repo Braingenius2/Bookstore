9db47bbc7dd368d5fcb8442639f7679d1880db1f
Braingenius2 <braingenius2@gmail.com>
2023-06-25 08:41:51 +0300

    Add CategoriesPage

---
+++ src/components/CategoriesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CategoriesPage = () => {
+  return (
+    <div className="container">
+      <h1>Categories</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non elit augue. Proin accumsan fringilla metus, ut lacinia orci blandit sit amet. Mauris convallis metus ut dui vulputate, eget interdum ipsum tincidunt. Ut at iaculis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed condimentum ipsum ut arcu posuere, a fermentum ex ultricies. Mauris auctor, turpis sit amet dignissim sollicitudin, risus lorem posuere mauris, ac fringilla felis neque non libero. Mauris ut justo euismod, dignissim turpis sed, condimentum dolor. Morbi a massa risus. Nunc at nunc sem.</p>
+      <p>Suspendisse quis elit ac lacus posuere aliquet. Vestibulum at eleifend dolor. Integer sit amet mauris ipsum. Nulla pellentesque ex vitae magna blandit, vel malesuada neque luctus. Fusce ac congue justo. Praesent vitae lorem a velit suscipit tincidunt nec a dui. Donec finibus, est nec volutpat efficitur, lectus massa sagittis mi, vitae faucibus eros purus at metus. Phasellus aliquam tellus metus, et posuere sem eleifend at. Curabitur at augue ac metus laoreet consequat at eu velit. Proin eu volutpat erat. Maecenas eget arcu at lorem lacinia ullamcorper. Praesent ut metus vitae sapien congue vulputate.</p>
+    </div>
+  );
+};
+
+export default CategoriesPage;
