fcd4df1d2ae088ec5b2b482f26546eeffd9f049a
Braingenius2 <braingenius2@gmail.com>
2023-06-25 08:16:53 +0300

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Book from './Book';
 
 const BookList = ({ books, onDelete }) => (
@@ -8,5 +9,16 @@ const BookList = ({ books, onDelete }) => (
     ))}
   </div>
 );
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default BookList;
