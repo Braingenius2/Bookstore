3db639df9607a40eb1bb8e3989a61ba009cc1f07
Braingenius2 <braingenius2@gmail.com>
2023-06-25 08:45:49 +0300

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './components/Homepage';
+import HomePage from './components/HomePage';
 import CategoriesPage from './components/CategoriesPage';
 import Navbar from './components/Navbar';
 
